7f1b12f0470d383810825f12d4cf7b081a385f4a
Anggi Riyandi <anggi.riyandi@gmail.com>
2017-09-26 11:23:20 +0700

    tambah shopping list dan shopping edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,5 +12,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
 }
