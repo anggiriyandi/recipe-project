bc15ef65837a9ea1c36579c42422969b66699e9a
Anggi Riyandi <anggi.riyandi@gmail.com>
2017-09-27 13:30:58 +0700

    menambahkan attribut ingredient ke dalam model recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,27 @@
+import {Ingredient} from '../shared/ingredient.model';
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   constructor() { }
-
-  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD8tdsd7nkUOji1QsAgGw3DPmtAlBWn9V9VmEaLm0OMqNzZu7Yjg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD8tdsd7nkUOji1QsAgGw3DPmtAlBWn9V9VmEaLm0OMqNzZu7Yjg')
-  ];
+    private recipes: Recipe[] = [
+      new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty Schnitzel - just awesome!',
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('French Fries', 20)
+        ]),
+      new Recipe('Big Fat Burger',
+        'What else you need to say?',
+        'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1)
+        ])
+    ];
 
   getRecipes() {
     return this.recipes;
